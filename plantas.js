a416eef3d36777a38fc73fdca3a2de07ca20e763
Anderson Guizan Silva <anderson.arquitetura@gmail.com>
2026-04-14 23:55:04 -0300

    Delete plantas.js

--- plantas.js
+++
@@ -1,8 +0,0 @@
-const plantas = [
-  { img: "plant-01.jpg" },
-  { img: "plant-02.jpg" },
-  { img: "plant-03.jpg" },
-  { img: "plant-04.jpg" },
-  { img: "plant-05.jpg" },
-  { img: "plant-06.jpg" }
-];
