3027e9f3cf10a692aaa9d19ae1c08582dc810715
Anderson Guizan Silva <anderson.arquitetura@gmail.com>
2026-04-14 22:16:19 -0300

    Add files via upload

---
+++ artigos.js
@@ -0,0 +1,10 @@
+const artigos = [
+  {
+    img: "art-01.jpg",
+    titulo: "Palácio Monroe",
+    link: "https://www.carlosromero.com.br/2021/02/a-triste-historia-de-um-palacio-demolido.html" 
+  },
+   {
+    tipo: "placeholder"
+  }
+];
---
+++ livros.js
@@ -0,0 +1,27 @@
+const livros = [
+  {
+    img: "liv-01.jpg",
+    titulo: "A Promessa da Guanabara",
+    sinopse: `No Rio de Janeiro de 1958, enquanto o rádio anunciava o
+primeiro título mundial e a Bossa Nova surgia como um
+sussurro nas janelas de Copacabana, um menino começava a
+desenhar sua própria história. Entre o barulho dos bondes
+que partiam e o silêncio dos novos edifícios que subiam, ele
+cresceu observando a alma de uma cidade em constante
+metamorfose.
+
+Em um relato que cruza décadas, este livro é uma arqueologia
+dos afetos. Das mãos do avô que “sabiam pensar” às ladeiras
+de pedra da Praça XV, acompanhamos uma trajetória que
+sobrevive entre o concreto e a saudade. Uma obra sobre o que
+o tempo apaga, o que a tecnologia transforma e o que o
+coração se recusa a esquecer — um enigma que só se
+completa quando a última página é escrita.`,
+    link: "https://link-do-livro.com"
+  },
+  {
+    img: "liv-02.jpg",
+    sinopse: "Aqui entra a sinopse do livro 2.",
+    link: "https://link-do-livro.com"
+  }
+];
